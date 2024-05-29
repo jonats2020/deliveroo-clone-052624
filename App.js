c21a146112839601fb84bec84b2562dbc6d96403
Nats <jonathan32587@yahoo.com>
2024-05-29 11:34:43 +0800

    added redux and implemented basket store

--- App.js
+++ App.js
@@ -3,16 +3,22 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import Homescreen from './src/screens/Homescreen';
 import RestaurantScreen from './src/screens/RestaurantScreen';
+import { Provider } from 'react-redux';
+import { store } from './store';
+
+
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Homescreen} />
-        <Stack.Screen name="Restaurant" component={RestaurantScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={Homescreen} />
+          <Stack.Screen name="Restaurant" component={RestaurantScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 }
